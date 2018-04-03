fd9a105b1836e32cb81a25ba8eeb84534bca1685
Tony Ta <tonyta.tt@gmail.com>
2018-04-03 13:55:01 -0700

    Allow projects to define their own Rubocop config
    The fix-db-schema-conflicts gem currently does not support rubocop
    v0.54.0. The maintainer is considering solutions to allow projects to
    set the rubocop configuration used by fix-db-schema-conflicts.
    
      https://github.com/jakeonrails/fix-db-schema-conflicts/issues/28

--- lib/fix-db-schema-conflicts.rb
+++ lib/fix-db-schema-conflicts.rb
@@ -1,5 +1,21 @@
-require 'fix_db_schema_conflicts/schema_dumper.rb'
+require 'fix_db_schema_conflicts/schema_dumper'
+require 'fix_db_schema_conflicts/autocorrect_configuration'
+
+require 'fix_db_schema_conflicts/railtie' if defined?(Rails)
 
 module FixDBSchemaConflicts
-  require 'fix_db_schema_conflicts/railtie' if defined?(Rails)
+  class << self
+    def rubocop_config=(config_path)
+      @rubocop_config = config_path
+    end
+
+    def rubocop_config
+      @rubocop_config || default_config
+    end
+
+    def default_config
+      relative_path = AutocorrectConfiguration.load
+      File.expand_path("../#{relative_path}", __dir__)
+    end
+  end
 end
--- lib/fix_db_schema_conflicts/tasks/db.rake
+++ lib/fix_db_schema_conflicts/tasks/db.rake
@@ -11,8 +11,7 @@ namespace :db do
       else
         "#{Rails.root}/db/schema.rb"
       end
-      autocorrect_config = FixDBSchemaConflicts::AutocorrectConfiguration.load
-      rubocop_yml = File.expand_path("../../../../#{autocorrect_config}", __FILE__)
+      rubocop_yml = FixDBSchemaConflicts.rubocop_config
       `bundle exec rubocop --auto-correct --config #{rubocop_yml} #{filename.shellescape}`
     end
   end
